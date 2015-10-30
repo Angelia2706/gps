5a4ca1bc21e9f9dd50aca42d2597a9ee8e152d21
Angelia2706 <karthikaaravind53@yahoo.in>
2015-10-30 22:28:36 +0530

    commit msg

---
+++ app/src/main/java/com/example/amala/gps/GPSTracker.java
@@ -0,0 +1,39 @@
+package com.example.amala.gps;
+
+import android.app.Service;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+
+/**
+ * Created by amala on 19/10/15.
+ */
+public class GPSTracker extends Service implements LocationListener {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
